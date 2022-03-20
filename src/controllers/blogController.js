7064098f3a0e1efb3ea881fd9b0d0a91044ecff5
ShubhenduSukul <shubhendu7870608936@mail.com>
2022-03-20 22:39:28 +0530

    Blogging

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -22,29 +22,81 @@ const loginUser = async function (req, res) {
 }
 
 
+// const createBlog = async function (req, res) {
+//   try {
+//     let blog = req.body
+//     if (Object.keys(blog).length == 0)
+//       return res.status(400).send({ status: false, msg: "No data to create a blog" })
+
+//     let author_id = blog.authorId
+//     if (!author_id)
+//       return res.status(400).send({ status: false, msg: "Author Id should be present in the blog data" })
+
+//     if (!ObjectId.isValid(author_id))
+//       return res.status(404).send({ status: false, msg: "AuthorId invalid" });
+
+//     let validAuthor = await AuthorModel.findById(author_id)
+//     if (!validAuthor)
+//       return res.status(404).send({ status: false, msg: "Author data not found" });
+
+//     let blogCreated = await BlogModel.create(blog)
+//     return res.status(201).send({ status: true, data: blogCreated })
+//   } catch (error) {
+//     return res.status(500).send({ status: false, msg: error.message })
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const createBlog = async function (req, res) {
   try {
     let blog = req.body
-    if (Object.keys(blog).length == 0)
+    if (Object.keys(blog).length == 0) {
       return res.status(400).send({ status: false, msg: "No data to create a blog" })
-
+    }
     let author_id = blog.authorId
-    if (!author_id)
+    if (!author_id) {
       return res.status(400).send({ status: false, msg: "Author Id should be present in the blog data" })
-
-    if (!ObjectId.isValid(author_id))
+    }
+    if (!ObjectId.isValid(author_id)) {
       return res.status(404).send({ status: false, msg: "AuthorId invalid" });
-
+    }
     let validAuthor = await AuthorModel.findById(author_id)
-    if (!validAuthor)
+    if (!validAuthor) {
       return res.status(404).send({ status: false, msg: "Author data not found" });
-
+    }
+    let token = req.headers["x-api-key"]
+    let decodedToken = jwt.verify(token, "room 37");
+    let authorLoggedIn = decodedToken.authorId
+    if (authorLoggedIn != author_id) {
+      return res.status(403).send({ status: false, msg: "Author logged in cannot create blog with another author's Id" })
+    }
+    let is_published = blog.isPublished
+    if (is_published == true) {
+      blog.publishedAt = new Date()
+      let blogCreated = await BlogModel.create(blog)
+      return res.status(201).send({ status: true, data: blogCreated })
+    }
     let blogCreated = await BlogModel.create(blog)
     return res.status(201).send({ status: true, data: blogCreated })
   } catch (error) {
     return res.status(500).send({ status: false, msg: error.message })
   }
 }
+
 
 
 const getBlog = async function (req, res) {
